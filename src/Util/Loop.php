2044f669b16be68897701c1053032ce1f19efa90
wangjian <1636801376@qq.com>
2017-12-19 09:52:01 +0800

    优化自旋算法

--- src/Util/Loop.php
+++ src/Util/Loop.php
@@ -5,6 +5,25 @@ use Wangjian\Lock\Exception\TimeoutException;
 
 class Loop {
     /**
+     * 最大重试次数，如果超过这个次数还未获取到锁，则进行休眠
+     * @var int
+     */
+    protected $maxTries = 5;
+
+    //随机休眠一段时间，减小脑裂的可能性
+    /**
+     * 休眠最小微秒数
+     * @var int
+     */
+    protected $minSleep = 50000;
+
+    /**
+     * 休眠最大微秒数
+     * @var int
+     */
+    protected $maxSleep = 100000;
+
+    /**
      * 超时时间，0表示不进行超时检测
      * @var int
      */
@@ -27,6 +46,7 @@ class Loop {
     public function loop(callable $until) {
         $start = microtime(true);
         $end = $start + $this->timeout;
+        $retries = 0;
 
         while(true) {
             $result = call_user_func($until);
@@ -39,6 +59,12 @@ class Loop {
             $now = microtime(true);
             if($this->timeout > 0 && $now > $end) {
                 throw new TimeoutException('spin lock timed out');
+            }
+
+            //如果超过最大重试次数，休眠
+            if(++$retries >= $this->maxTries) {
+                $retries = 0;
+                usleep(rand($this->minSleep, $this->maxSleep));
             }
         }
     }
